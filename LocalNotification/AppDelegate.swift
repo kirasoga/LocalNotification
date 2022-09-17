49288aa1057fac6b83e6bc510b69338476674856
KiraSoga <sogakira0202@gmail.com>
2022-09-17 13:37:14 +0900

    バージョン修正

--- LocalNotification/AppDelegate.swift
+++ LocalNotification/AppDelegate.swift
@@ -8,18 +8,16 @@ import UIKit
 import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate  {
     
     var window: UIWindow?
     
-    internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // アプリのバッジの更新、サウンド、アラートの通知に関してユーザへ確認
-        let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .badge, .sound]) {(granted, error) in
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) {(granted, error) in
             if granted {
                 print("許可する")
+                UNUserNotificationCenter.current().delegate = self
             } else {
                 print("許可しない")
             }
@@ -47,5 +45,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // フォアグラウンドの場合でも通知を表示する
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .sound,])
     }
 }
--- LocalNotification/ViewController.swift
+++ LocalNotification/ViewController.swift
@@ -6,29 +6,29 @@
 //
 
 import UIKit
-import UserNotifications
 
-class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+class ViewController: UIViewController {
     
     // 通知を作成
     @IBAction func setNotification(_ sender: Any) {
         // タイトル、本文、サウンド設定の保持
         let content = UNMutableNotificationContent()
-        content.title = "まもなく(何日に登録された)TODOLISTのデータが消えます"
-//        content.subtitle = "10秒経過しました"
+        content.title = "タイトル"
+        content.subtitle = "サブタイトル"
         content.body = "タップしてアプリを開いてください"
         content.sound = UNNotificationSound.default
         
         // seconds後に起動するトリガーを保持
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10,
+                                                        repeats: false)
         
         // 識別子とともに通知の表示内容とトリガーをrequestに内包する
-        let request = UNNotificationRequest(identifier: "Timer", content: content, trigger: trigger)
+        let request = UNNotificationRequest(identifier: "Timer",
+                                            content: content,
+                                            trigger: trigger)
         
-        // UNUserNotificationCenterにrequestを加える
-        let center = UNUserNotificationCenter.current()
-        center.delegate = self
-        center.add(request) { (error) in
+        // UNUserNotificationCenterにrequest
+        UNUserNotificationCenter.current().add(request) { (error) in
             if let error = error {
                 print(error.localizedDescription)
             }
@@ -43,11 +43,5 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    // フォアグラウンドの場合でも通知を表示する
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        //アプリ内でのalertは推奨されてないが、使いたいので入れている
-        completionHandler([.alert, .badge, .sound])
     }
 }
